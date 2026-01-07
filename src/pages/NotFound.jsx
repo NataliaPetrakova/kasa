0a51f007308e3a5033fb4b1f10fe4dfbfbb254ad
« PetrakovaNatalia » <Natalia.p@live.fr>
2026-01-07 13:40:23 +0100

    Add styled 404 error page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom'
+
 function NotFound() {
   return (
-    <div>
-      <h1>404 - Page non trouvée</h1>
+    <div className="not-found">
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/">Retourner sur la page d'accueil</Link>
     </div>
   )
 }
